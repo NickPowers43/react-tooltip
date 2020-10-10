5a5d25ce7618886c6f14deeb2c24dfc030cd555c
Nicholas Powers <nickpowers43@gmail.com>
2020-10-10 13:17:45 -0400

    Bind and cleanup event listeners using React's effects

--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,46 @@
 
 import * as React from 'react';
 
-export function ToolTip(props) {
+export function ToolTip(props: React.PropsWithChildren<{anchorElement: HTMLElement}>) {
   
-    const { anchorElement } = props;
+  const { anchorElement } = props;
+  
+  const [ anchor, setAnchor ] = React.useState<HTMLElement>(null);
+  const [ show, setShow ] = React.useState(false);
+
+  // Effect for binding mouse enter/leave event listeners
+  React.useEffect(
+    () => {
+      if (!anchorElement)
+        return;
+
+      const enterListener = e => setShow(true);
+      const leaveListener = e => setShow(false);
+
+      anchorElement.addEventListener('mouseenter', enterListener);
+      anchorElement.addEventListener('mouseleave', leaveListener);
+
+      // clean up event listeners
+      return () => {
+        anchorElement.removeEventListener('mouseenter', enterListener);
+        anchorElement.removeEventListener('mouseleave', leaveListener);
+      }
+    },
+    [anchorElement]
+  );
+  
+  if (anchorElement) {
+    const bounds = anchorElement.getBoundingClientRect();
+    const verticalCenter = (bounds.top + bounds.bottom) / 2;
+    const x = bounds.right + 5;
     
-    const [ show, setShow ] = React.useState(true);
+    if (!show)
+      return null;
     
-    if (anchorElement) {
-      const bounds = anchorElement.getBoundingClientRect();
-      const verticalCenter = (bounds.top + bounds.bottom) / 2;
-      const x = bounds.right + 5;
-      
-      anchorElement.addEventListener('mouseenter', e => {
-        setShow(true);
-      });
-      
-      anchorElement.addEventListener('mouseleave', e => {
-        setShow(false);
-      });
-      
-      if (!show)
-        return null;
-      
-      return (
-        <div className="tooltip" style={{left: x, top: verticalCenter + 'px'}}>Tooltip</div>
-      );
-    }
-    
-    return null;
+    return (
+      <div className="tooltip" style={{left: x, top: verticalCenter + 'px'}}>Tooltip</div>
+    );
   }
+  
+  return null;
+}
